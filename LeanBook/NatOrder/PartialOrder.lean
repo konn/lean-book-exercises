2c86666723fa5f8f54e594ab478108edf0ddcbd1
Hiromi ISHII <konn.jinro@gmail.com>
2025-09-07 12:26:17 +0900

    partial ord

--- LeanBook/NatOrder/PartialOrder.lean
+++ LeanBook/NatOrder/PartialOrder.lean
@@ -26,3 +26,22 @@ instance : Trans (· ≤ · : MyNat → MyNat → Prop) (· < ·) (· < ·) wher
 
 instance : Trans (· < · : MyNat → MyNat → Prop) (· ≤ ·) (· < ·) where
   trans := MyNat.lt_of_lt_of_le
+
+theorem MyNat.le_antisymm (h₁: n ≤ m) (h₂: m ≤ n) : n = m := by
+  induction h₁ with
+  | refl => rfl
+  | @step m h ih =>
+    have : n < n := calc
+      _ ≤ m     := by exact h
+      _ < m + 1 := by notation_simp; rfl
+      _ ≤ n     := by exact h₂
+    simp at this
+
+example (a b : MyNat): a < b ∨ a = b → a ≤ b := by
+  intro h
+  cases h with
+  | inl h' => exact MyNat.le_of_lt h'
+  | inr h' =>
+      apply MyNat.le_of_eq_or_lt
+      left
+      assumption
